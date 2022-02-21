4688f7fedc39ac051aca1530641ea248db8f76b3
omal777 <egorlg3gs@gmail.com>
2022-02-21 11:27:43 +0200

    привел в порядок согласно с рубокопом.

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
-#test
+# frozen_string_literal: true
+
+# test
 # Создадаим массив с набором достоинств
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = %w[Diamonds Hearts Clubs Spades]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
